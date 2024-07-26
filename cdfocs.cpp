d6de174b22544754e49fafb8aaf6e46646faf360
chaturvedi2004 <145530671+chaturvedi2004@users.noreply.github.com>
2024-07-26 07:53:16 +0530

    first commit

---
+++ cdfocs.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*int main(){
+    int t;
+    cin>>t;
+    while(t--){
+       int x,y;
+       cin>>x>>y;
+       if(((x*1) +(y*4))<=15){
+        cout<<"1"<<endl;
+       }
+       else{
+        cout<<((((x*1) +(y*4))/15)+1)<<endl;
+       }
+    }
+    return 0;
+}
+int main(){
+        vector<int>arr{0,1,1,0,1,0,0,1,0,1,1,0};
+        int start =0;
+        int end =arr.size() -1;
+        int i=0;
+        while(i!=end){
+          if(arr[i]==0){
+            swap(arr[i],arr[start]);
+            start++;
+            i++;
+          }
+          else{
+            swap(arr[i],arr[end]);
+            end--;
+          }
+        
+        }
+        for(int i=0;i<arr.size();i++){
+            cout<<arr[i]<<" ";
+        }
+        return 0;
+
+
+
+      }*/
+      int main(){
+        cout<<"aman kumar"<<endl;
+      }
